c9a2bdc736c751f5202f36c2fb82c06bbeefaa7c
stevenmills <samills0608@gmail.com>
2017-06-15 18:37:24 -0400

    assignment-20-crud3: copmleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :question
   resources :posts
 
   get 'about' => 'weclome#about'
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true || false) }
-  describe "attributes" do
-    it "has title, body and resolved attributes" do
-      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: true || false)
+  context "attributes" do
+    let(:question) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
+
+    it "should respond to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "should respond to resolved" do
+      expect(question).to respond_to(:resolved)
     end
   end
 end
