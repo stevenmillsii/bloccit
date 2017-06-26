ecf2507cbc29fdf9724606d349e11454e5641a56
stevenmills <samills0608@gmail.com>
2017-06-26 16:58:49 -0400

    assignment-23-authentication: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format the users name" do
+      user.name = "bloc user"
+      user.save
+      expect(user.name).to eq "Bloc User"
+    end
   end
 
   describe "invalid user" do
